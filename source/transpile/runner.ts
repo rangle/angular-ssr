b9d033261cd40aa9751437ff923d1f5e2311c020
Christopher Bond <bond@Bond-MacBook.local>
2017-02-22 13:57:16 -0500

    Minor refactoring of transpile compiler

--- source/transpile/runner.ts
+++ source/transpile/runner.ts
@@ -2,20 +2,29 @@ import {TranspileResult} from './transpiler';
 
 import {transpileCache} from './cache';
 
+const Module = require('module');
+
 export const runner = <R>(module: NodeModule, source: string): TranspileResult<R> => {
-  let exports = undefined;
+  let executed = false;
 
-  const run = (): R => {
-    if (exports === undefined) {
-      exports = module.exports;
-
-      (<{_compile?}>module)._compile(source, module.filename);
-
-      transpileCache.write(module.id, {module, source, run: () => exports});
+  return <TranspileResult<R>> {
+    module,
+    source,
+    run: (): R => {
+      if (executed === false) {
+        compile(module, source);
+      }
+      return module.exports;
     }
-
-    return exports;
   };
+};
 
-  return <TranspileResult<R>> {module, source, run};
+const compile = (module: NodeModule, source: string): void => {
+  Module.prototype._compile.call(module, source, module.filename);
+
+  cache(module, source);
+};
+
+const cache = (module: NodeModule, source: string) => {
+  transpileCache.write(module.id, {module, source, run: () => module.exports});
 };
