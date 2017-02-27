47f70d2715b13971140b86cfc0d107faee1e8919
Christopher Bond <cb@clbond.org>
2017-02-27 17:04:15 -0500

    Clean up the destroy pattern for NgModuleRef instances

--- source/platform/module/bootstrap.ts
+++ source/platform/module/bootstrap.ts
@@ -35,12 +35,9 @@ export const compileModule = async <M>(moduleType: Type<M>): Promise<NgModuleFac
 export const bootstrapModuleFactory = async <M, R>(moduleFactory: NgModuleFactory<M>, execute: ModuleExecute<M, R>): Promise<R> => {
   const moduleRef = await platform.bootstrapModuleFactory<M>(moduleFactory);
   try {
-    const result = await Promise.resolve(execute(moduleRef));
-    moduleRef.destroy();
-    return result;
+    return await Promise.resolve(execute(moduleRef));
   }
-  catch (exception) {
+  finally {
     moduleRef.destroy();
-    throw exception;
   }
 };
