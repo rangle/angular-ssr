7a860a19f16eae206e3e1ea43bc2e61fbe4ea34d
Christopher Bond <cb@clbond.org>
2017-03-30 11:42:36 -0400

    Resolve timing issue with ApplicationBuilderFromSource tests

--- source/application/builder/tests/from-source.ts
+++ source/application/builder/tests/from-source.ts
@@ -1,54 +1,51 @@
 import {ApplicationBuilderFromSource} from '../from-source';
+import {Disposable} from '../../../disposable';
 
 import {templateDocument, getApplicationProject} from '../../../test/fixtures';
 
 describe('ApplicationFromSource', () => {
+  const disposables = new Array<Disposable>();
+
+  afterAll(() => disposables.forEach(d => d.dispose()));
+
   it('can compile a project from source and render it', async () => {
     const builder = new ApplicationBuilderFromSource(getApplicationProject('source/test/fixtures/application-basic-inline', 'BasicInlineModule'), templateDocument);
 
     const application = builder.build();
+    disposables.push(application);
 
-    try {
-      const snapshots = await application.prerender();
+    const snapshots = await application.prerender();
 
-      return new Promise((resolve, reject) => {
-        snapshots.subscribe(
-          r => {
-            expect(r.exceptions).not.toBeNull();
-            expect(r.exceptions.length).toBe(0);
-            expect(r.uri).toBe('http://localhost/');
-            expect(r.variant).toBeUndefined();
-            expect(r.applicationState).toBeUndefined();
-            const expr = /<application ng-version="([^"]+)"><div>Hello!<\/div><\/application>/;
-            expect(expr.test(r.renderedDocument)).toBeTruthy();
-            resolve();
-          },
-          exception => reject(exception));
-      });
-    }
-    finally {
-      application.dispose();
-    }
+    return new Promise((resolve, reject) => {
+      snapshots.subscribe(
+        r => {
+          expect(r.exceptions).not.toBeNull();
+          expect(r.exceptions.length).toBe(0);
+          expect(r.uri).toBe('http://localhost/');
+          expect(r.variant).toBeUndefined();
+          expect(r.applicationState).toBeUndefined();
+          const expr = /<application ng-version="([^"]+)"><div>Hello!<\/div><\/application>/;
+          expect(expr.test(r.renderedDocument)).toBeTruthy();
+          resolve();
+        },
+        exception => reject(exception));
+    });
   });
 
   it('can compile from source and render lazy-loaded routes', async () => {
     const builder = new ApplicationBuilderFromSource(getApplicationProject('source/test/fixtures/application-lazy-routed', 'BasicLazyRoutedModule'), templateDocument);
 
     const application = builder.build();
+    disposables.push(application);
 
-    try {
-      const snapshot = await application.renderUri('http://localhost/one');
+    const snapshot = await application.renderUri('http://localhost/one');
 
-      expect(snapshot.exceptions).not.toBeNull();
-      expect(snapshot.exceptions.length).toBe(0);
-      expect(snapshot.uri).toBe('http://localhost/one');
-      expect(snapshot.variant).toBeUndefined();
-      expect(snapshot.applicationState).toBeUndefined();
-      const expr = /<application ng-version="([^"]+)"><router-outlet><\/router-outlet><basic-lazy-component ng-version="([^"]+)">Lazy loaded component!<\/basic-lazy-component><\/application>/;
-      expect(expr.test(snapshot.renderedDocument)).toBeTruthy();
-    }
-    finally {
-     application.dispose();
-    }
+    expect(snapshot.exceptions).not.toBeNull();
+    expect(snapshot.exceptions.length).toBe(0);
+    expect(snapshot.uri).toBe('http://localhost/one');
+    expect(snapshot.variant).toBeUndefined();
+    expect(snapshot.applicationState).toBeUndefined();
+    const expr = /<application ng-version="([^"]+)"><router-outlet><\/router-outlet><basic-lazy-component ng-version="([^"]+)">Lazy loaded component!<\/basic-lazy-component><\/application>/;
+    expect(expr.test(snapshot.renderedDocument)).toBeTruthy();
   });
 });
