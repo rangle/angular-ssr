10a6673805ae8565e25b2c2c0bb30a0f7634011d
Christopher Bond <cb@clbond.org>
2017-04-09 14:53:20 -0400

    Resolve issues with package.json and CircleCI build

--- source/static/files.ts
+++ source/static/files.ts
@@ -1,16 +1,16 @@
-export namespace Files {
+export abstract class Files {
   // Angular CLI configuration file names
-  export const cli = ['angular-cli.json', '.angular-cli.json'];
+  static readonly cli = ['angular-cli.json', '.angular-cli.json'];
 
   // Filename used when writing route structures to disk (eg foo/index.html)
-  export const index = 'index.html';
+  static readonly index = 'index.html';
 
   // The possible names of node_modules folders (TODO(bond): Really inviolate or configurable?)
-  export const modules = 'node_modules';
+  static readonly modules = 'node_modules';
 
   // The names we will search when we look for project tsconfig.json files
-  export const tsconfig = ['tsconfig.app.json', 'tsconfig.json'];
+  static readonly tsconfig = ['tsconfig.app.json', 'tsconfig.json'];
 
   // Webpack configuration filenames sorted in precedence order
-  export const webpack = ['webpack.server.config.js', 'webpack.app.config.js', 'webpack.config.js'];
+  static readonly webpack = ['webpack.server.config.js', 'webpack.app.config.js', 'webpack.config.js'];
 }
