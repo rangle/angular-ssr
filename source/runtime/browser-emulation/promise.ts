95856ce7ed9594421cd578da33b1a78e577ccc0d
Christopher Bond <cb@clbond.org>
2017-04-13 11:25:18 -0400

    Use forceful zone.js bluebird patch to deal with webpack plugin issues

--- source/runtime/browser-emulation/promise.ts
+++ source/runtime/browser-emulation/promise.ts
@@ -5,3 +5,9 @@ const promise = Promise;
 const bluebird = require('bluebird'); // some webpack plugins use bluebird to disastrous effect
 
 bluebird.Promise = promise;
+
+import 'zone.js/dist/zone-bluebird';
+
+declare const Zone;
+
+Zone[Zone.__symbol__('bluebird')](bluebird);
